558e0d2cb851e6a3a83fa699fe4b1ad5f1ceb4f4
MikeUU332 <michael.ramshaw@utah.edu>
2026-05-12 23:57:08 -0600

    Update mcp_server_quack.py

--- LLM_Fine_Tuning_Presentation/mcp_server_quack.py
+++ LLM_Fine_Tuning_Presentation/mcp_server_quack.py
@@ -1,11 +1,6 @@
 """
-FastMCP quickstart example.
-
-Run from the repository root:
-    uv run examples/snippets/servers/fastmcp_quickstart.py
+FastMCP DuckDuckGo web search capable
 """
-
-# from mcp.server.fastmcp import FastMCP, http_app
 from fastmcp import FastMCP
 from fastapi import FastAPI
 import uvicorn
@@ -15,15 +10,9 @@ from ddgs import DDGS
 #mcp = FastMCP("Demo", json_response=True)
 mcp = FastMCP("QuackPower")
 
-# Add an addition tool
-#@mcp.tool()
-#def add(a: int, b: int) -> int:
-#    """Add two numbers"""
-#    return a + b + b + a #Yes this is wrong on purpose to see if it really calls the tool
-
 @mcp.tool()
 def duckduckgo_search(query: str, max_results: int = 5) -> str:
-    print("calling duck army", flush=True)
+    # print("Server verifying duck army is called into action", flush=True)
     """Search the web with DuckDuckGo and return the top results."""
     if not query.strip():
         return "Please provide a search query."
@@ -59,31 +48,12 @@ def duckduckgo_search(query: str, max_results: int = 5) -> str:
 #    print("Calling get_current_time", flush=True)
 #    return datetime.now().strftime("%d/%m/%Y")
 
-# Add a dynamic greeting resource
-#@mcp.resource("greeting://{name}")
-#def get_greeting(name: str) -> str:
-#    """Get a personalized greeting"""
-#    return f"Greetings, solicitations and felicitations, {name}!"
-
-# Add a prompt
-#@mcp.prompt()
-#def greet_user(name: str, style: str = "friendly") -> str:
-#    """Generate a greeting prompt"""
-#    styles = {
-#        "friendly": "Please write a warm, friendly greeting",
-#        "formal": "Please write a formal, professional greeting",
-#        "casual": "Please write a casual, relaxed greeting",
-#    }
-
-#    return f"{styles.get(style, styles['friendly'])} for someone named {name}."
-
 #FastAPI for conversion if needed
 #mcp_app = mcp.http_app(path='/mcp')
 #app = FastAPI(title="Quack Powered Searches", lifespan=mcp_app.lifespan)
 #app.mount('/mcp', mcp.streamable_http_app())
 #app.mount("/ddgs", mcp_app)
-#for route in app.routes:
-#       print(route.path, route.name)
+
 # Run with streamable HTTP transport
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
